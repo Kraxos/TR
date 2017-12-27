8abb3207badfc65947ff2f3b1609382b40c9614e
Kraxos <30649419+Kraxos@users.noreply.github.com>
2017-12-27 21:09:38 +0100

    Update config.py

--- LoPy Gateway/config.py
+++ LoPy Gateway/config.py
@@ -4,24 +4,19 @@ import machine
 import ubinascii
 
 WIFI_MAC = ubinascii.hexlify(machine.unique_id()).upper()
-# Set  the Gateway ID to be the first 3 bytes of MAC address + 'FFFE' + last 3 bytes of MAC address
+# El Gateway ID serán los 3 primeros bytes de la dirección MAC + 'FFEE' + últimos 3 bytes de la dirección MAC
 GATEWAY_ID = WIFI_MAC[:6] + "FFFE" + WIFI_MAC[6:12]
 
 SERVER = 'router.eu.thethings.network'
-PORT = 1700 ##1901
+PORT = 1700 
 
 NTP = "pool.ntp.org"
 NTP_PERIOD_S = 3600
 
-WIFI_SSID = 'WLAN_C670'
-WIFI_PASS = 'BQ18PSIBAMK7MDBO8NDK'
+WIFI_SSID = 'WLAN_C670' # Nombre de la red wifi en la que se ha de conectar el gateway
+WIFI_PASS = 'BQ18PSIBAMK7MDBO8NDK' # Contraseña de dicha red wifi
 
-# for EU868
-LORA_FREQUENCY = 868100000
-LORA_GW_DR = "SF7BW125" # DR_5
+# Europa: EU868
+LORA_FREQUENCY = 868100000 # Frecuencia = 868.1 MHz
+LORA_GW_DR = "SF7BW125" #SF7 => Spreading Factor = 7
 LORA_NODE_DR = 5
-
-# for US915
-# LORA_FREQUENCY = 903900000
-# LORA_GW_DR = "SF7BW125" # DR_3
-# LORA_NODE_DR = 3
